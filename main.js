e58c5906b39fcc381e9918ddee1359ce4f6af055
Dennis_Vargas <dennis.vargas@wsu.edu>
2018-02-26 13:14:32 -0800

    Adds a created method that loads the seat-map.json file and sets all objects to static.
    Signed-off-by: DennisVargas <dennis.vargas@wsu.edu>

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
+/*jshint esversion: 6 */
+const fabCanvas = new fabric.Canvas('c');
 
+fabCanvas.setWidth(window.innerWidth);
+fabCanvas.setHeight(window.innerHeight);
+
+var vm = new Vue({
+    el: '#vue-app',
+    data:{
+        mapData:{},
+    },
+    methods:{
+        
+    },
+    created(){
+        // loads a canvas instance from the data store in seat-map.json
+        $.getJSON( "./seat-map.json", function( data ) {
+            fabCanvas.loadFromJSON(data);
+            console.log("sup");
+            fabCanvas.forEachObject(function(object){ 
+                object.selectable = false; 
+            });
+        });
+
+        
+    }
+});
