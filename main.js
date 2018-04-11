8f0af2f0deb52befc2dd7d7a0a75f8a17b013fa4
Christopher Feener <christopher.feener@wsu.edu>
2018-04-10 21:53:47 -0700

    Began implementing taking from the JSON data

--- main.js
+++ main.js
@@ -6,25 +6,49 @@ fabCanvas.setHeight(window.innerHeight);
 
 var vm = new Vue({
     el: '#vue-app',
-    data:{
-        mapData:{},
+    data: {
+        mapData: {},
     },
-    methods:{
-        
+    methods: {
+        loadDataToFabCanvas(dataArray) {
+            var element =(dataArray[1]);
+            var price = element.price;
+            if (price == null) {
+                console.log("loadDataToFabCanvas: No price");
+                fabCanvas.loadFromJSON(element);
+                console.log(element);
+            } else {
+                console.log("loadDataToFabCanvas: GOT PRICE! YAY!");
+                console.log(price);
+            }
+        }
     },
-    created(){
+    created() {
         // loads a canvas instance from the data store in seat-map.json
-        $.getJSON( "./seat-map.json", function( data ) {
-            fabCanvas.loadFromJSON(data);
-            console.log("sup");
-            fabCanvas.forEachObject(function(object){ 
+        $.getJSON("./seat-map.json", function (dataArray) {
+            console.log(dataArray);
+            //vm.loadDataToFabCanvas(dataArray);
+            var element =(dataArray[1]);
+            var price = element.price;
+            if (price == null) {
+                console.log("loadDataToFabCanvas: No price");
+                //fabCanvas.loadFromJSON(element);
+                fabCanvas.add(element);
+                console.log(element);
+            } else {
+                console.log("loadDataToFabCanvas: GOT PRICE! YAY!");
+                console.log(price);
+            }
+            fabCanvas.forEachObject(function (object) {
+                console.log("Object");
+                console.log(object);
                 object.lockMovementX = true;
                 object.lockMovementY = true;
                 object.lockRotation = true;
-                object.selectable = true; 
+                object.selectable = true;
             });
         });
 
-        
+
     }
 });
