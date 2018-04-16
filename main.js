947e3744f50d6f90484a20a5b2a946d91e97b37f
Christopher Feener <christopher.feener@wsu.edu>
2018-04-15 19:44:48 -0700

    Unpacked data and objects therein. Work on disabling scaling.

--- main.js
+++ main.js
@@ -25,12 +25,12 @@ var vm = new Vue({
     },
     created() {
         // loads a canvas instance from the data store in seat-map.json
-        $.getJSON("./seat-map.json", function (dataArray) {
-            console.log(dataArray);
+        $.getJSON("./seat-map.json", function (data) {
+            console.log(data);
             //vm.loadDataToFabCanvas(dataArray);
-            var element =(dataArray[1]);
-            var price = element.price;
-            if (price == null) {
+            //var element =(data[1]);
+            //var price = element.price;
+            /*if (price == null) {
                 console.log("loadDataToFabCanvas: No price");
                 //fabCanvas.loadFromJSON(element);
                 fabCanvas.add(element);
@@ -38,17 +38,54 @@ var vm = new Vue({
             } else {
                 console.log("loadDataToFabCanvas: GOT PRICE! YAY!");
                 console.log(price);
-            }
-            fabCanvas.forEachObject(function (object) {
+            }*/
+            fabCanvas.loadFromJSON(data);
+            /*fabCanvas.forEachObject(function (object) {
                 console.log("Object");
                 console.log(object);
                 object.lockMovementX = true;
                 object.lockMovementY = true;
                 object.lockRotation = true;
                 object.selectable = true;
-            });
+            });*/
+            var groups = fabCanvas.getObjects();
+            console.log(groups);
+            groups.forEach((section) => {
+                console.log(section);
+                var sectionObjects = Array.from(section.getObjects());
+                sectionObjects.forEach((object) => {
+                    //CNF: Disable scaling!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                    object.hasControls = false;
+                    object.hasBorders = false;
+                    object.lockMovementX = true;
+                    object.lockMovementY = true;
+                    object.lockRotation = true;
+                    object.selectable = true;
+                    if (section.sectionType == "generalArea") {
+                        console.log("generalArea");
+                        if (object.type == "rect") {
+                            console.log("Object rect");
+                        }
+                    }
+                })
+            })
+            /*// ungroup objects in group
+            var groupItems = [];
+            var ungroup = function (group) {
+                // console.log("in ungroup()");
+                groupItems = group._objects;
+                group._restoreObjectsState();
+                
+
+                fabCanvas.remove(group);
+                for (var i = 0; i < groupItems.length; i++) {
+                    fabCanvas.add(groupItems[i]);
+                    items[i].dirty = true;
+                    fabCanvas.item(fabCanvas.size()-1).hasControls = false;
+                }
+                // if you have disabled render on addition
+                fabCanvas.renderAll();
+            };*/
         });
-
-
     }
 });
